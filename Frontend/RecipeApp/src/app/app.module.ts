33568b4941b8926f8a17541633e2298e601b08c9
joeldevos2008 <joeldevos2008@gmail.com>
2018-09-05 19:57:55 -0400

    committing....

--- Frontend/RecipeApp/src/app/app.module.ts
+++ Frontend/RecipeApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -28,7 +29,8 @@ import { AboutComponent } from './components/about/about.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
 
   ],
   providers: [AuthService],
