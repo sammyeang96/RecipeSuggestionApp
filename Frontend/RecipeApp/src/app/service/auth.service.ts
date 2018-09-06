204242cbb1e2c622975a3ef54916b9d643931cf6
Dylancorbus <Dylancorbus@outlook.com>
2018-09-06 01:13:57 -0400

    commit search results display on the screen but i need to learn how to parse the json

--- Frontend/RecipeApp/src/app/service/auth.service.ts
+++ Frontend/RecipeApp/src/app/service/auth.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Recipe } from '../models/Recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   link: string;
-  recipe: string[];
+  recipe: JSON;
   constructor(private http: HttpClient) { }
 
   searchRecipes(search: string) {
-    return this.http.get<any>(`https://api.edamam.com/search?q=${search}&app_id=aeab67c5&app_key=43503b89948d858f171e29557e629321`);
+    
+    return this.http.get<JSON>(`https://api.edamam.com/search?q=${search}&app_id=aeab67c5&app_key=43503b89948d858f171e29557e629321`);
   }
 }
