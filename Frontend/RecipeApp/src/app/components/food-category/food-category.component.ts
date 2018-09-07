c2d4c5e4cb229c624354097acaec4f87c0c392a8
Dylancorbus <Dylancorbus@outlook.com>
2018-09-07 16:18:46 -0400

    m

--- Frontend/RecipeApp/src/app/components/food-category/food-category.component.ts
+++ Frontend/RecipeApp/src/app/components/food-category/food-category.component.ts
@@ -6,7 +6,7 @@ import { Ingredient } from '../../models/Ingredient.model';
 @Component({
   selector: 'app-food-category',
   templateUrl: './food-category.component.html',
-  // styleUrls: ['./food-category.component.css']
+  styleUrls: ['./food-category.component.css']
 })
 export class FoodCategoryComponent implements OnInit {
 
