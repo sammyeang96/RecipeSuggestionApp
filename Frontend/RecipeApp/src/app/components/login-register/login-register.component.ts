12ac9fe8ffa11f432c450f43d145d1c8c504022f
joeldevos2008 <joeldevos2008@gmail.com>
2018-09-07 15:10:43 -0400

    some stuff with login/register

--- Frontend/RecipeApp/src/app/components/login-register/login-register.component.ts
+++ Frontend/RecipeApp/src/app/components/login-register/login-register.component.ts
@@ -12,6 +12,13 @@ export class LoginRegisterComponent implements OnInit {
 
   private username: string;
   private password: string;
+
+  private fullName: string;
+  private newUsername: string;
+  private newPassword: string;
+  private confirmPassword: string;
+  private newEmail: string;
+
   show = false;
   hidden = true;
 
@@ -31,19 +38,34 @@ export class LoginRegisterComponent implements OnInit {
 
     if (this.username == null || this.password == null) {
       alert('please enter in something.');
-    }
-    else {
-      console.log("printing info... ")
+    } else {
+      console.log('printing info... ');
       console.log(this.username);
       console.log(this.password);
 
       this.hidden = !this.hidden;
       this.show = !this.show;
-
-      //SOME LOGIN LOGIC
-
+      // does nothing yet
         }
-
     }
+
+    registerUser() {
+
+      if (this.fullName == null || this.newUsername == null || this.newPassword == null || this.confirmPassword == null
+        || this.newEmail == null) {
+        alert('please fill  in all fields');
+      } else {
+        console.log('printing info... ');
+        console.log(this.fullName);
+        console.log(this.newUsername);
+        console.log(this.newPassword);
+        console.log(this.confirmPassword);
+        console.log(this.newEmail);
+
+        this.hidden = !this.hidden;
+        this.show = !this.show;
+        // does nothing yet
+          }
+      }
 
 }
