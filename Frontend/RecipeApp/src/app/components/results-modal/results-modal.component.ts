bb03a1b69b12316ae1da44b6e0678c47643445ff
Dylancorbus <Dylancorbus@outlook.com>
2018-09-06 22:23:41 -0400

    m

--- Frontend/RecipeApp/src/app/components/results-modal/results-modal.component.ts
+++ Frontend/RecipeApp/src/app/components/results-modal/results-modal.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule, Router } from '../../../../node_modules/@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-results-modal',
   templateUrl: './results-modal.component.html',
   styleUrls: ['./results-modal.component.css']
+  
 })
 export class ResultsModalComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal,
+    private route: RouterModule,
+    private router: Router,
+    private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  openLoginModal(content) {
-   this.modalService.open(content, { size: 'lg' });
-  }
+  // openLoginModal(content) {
+  //  this.modalService.open(content, { size: 'lg' });
+  // }
 }
