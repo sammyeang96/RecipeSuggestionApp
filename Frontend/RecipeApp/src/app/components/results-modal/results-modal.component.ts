6a7328dd2d7ff2d9cb04fc2e567d20ac93a03b07
10michael30 <mikeclutch@yahoo.com>
2018-09-07 16:12:47 -0400

    o

--- Frontend/RecipeApp/src/app/components/results-modal/results-modal.component.ts
+++ Frontend/RecipeApp/src/app/components/results-modal/results-modal.component.ts
@@ -3,7 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-results-modal',
   templateUrl: './results-modal.component.html',
-  styleUrls: ['./results-modal.component.css']
+ // styleUrls: ['./results-modal.component.css']
 })
 export class ResultsModalComponent implements OnInit {
 
