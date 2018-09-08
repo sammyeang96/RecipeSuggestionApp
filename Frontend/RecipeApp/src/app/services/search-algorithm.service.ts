e4b14bc6aeb1a9992b1e267fb8768496bfdb6033
JeffreyZhong <jeffreyzhong1@gmail.com>
2018-09-08 13:18:00 -0400

    searchalg

--- Frontend/RecipeApp/src/app/services/search-algorithm.service.ts
+++ Frontend/RecipeApp/src/app/services/search-algorithm.service.ts
@@ -9,45 +9,42 @@ export class SearchAlgorithmService {
   constructor(private http: HttpClient) { }
 
   private searchUrl: string = "";
-  private selectedPantry: Ingredient[];
+  // private selectedPantry: Ingredient[];
  
 
-  randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  // randomInt(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+
+  // searchPantryRecipes(searchArray: Ingredient[]) {
+  //   let starchcount : number;
+  //   let meatCount : number;
+  //   let fruitCount : number;
+  //   let vegetableCount : number;
+  //   let dairyCount : number;
+  //   let spicesCount : number;
+  
+  //   let random: number = this.randomInt(0, searchArray.length)
+  //   while (this.selectedPantry.length <= 5)
+
+  //     for (let i = random; i < searchArray.length; i++) {
+  //       if (searchArray[i].weight >12) {
+
+  //         this.selectedPantry.add
+
+  //       }
+  //     }
+
+
+  // }
 
   searchPantryRecipes(searchArray: Ingredient[]) {
-    let starchcount : number;
-    let meatCount : number;
-    let fruitCount : number;
-    let vegetableCount : number;
-    let dairyCount : number;
-    let spicesCount : number;
-  
-    let random: number = this.randomInt(0, searchArray.length)
-    while (this.selectedPantry.length <= 5)
-
-      for (let i = random; i < searchArray.length; i++) {
-        if (searchArray[i].weight >12) {
-
-          this.selectedPantry.add
-
-        }
-      }
-
-
-  }
-
-
-
-
-  searchSelectedPantryRecipes(searchArray: Ingredient[]) {
 
     for (let i = 1; i < searchArray.length; i++) {
       this.searchUrl = searchArray[0].name;
       this.searchUrl = this.searchUrl + "," + searchArray[i].name;
     }
-    return this.http.get<any>(`https://api.edamam.com/search?q=${this.searchUrl}&app_id=aeab67c5&app_key=43503b89948d858f171e29557e629321&ingr=${searchArray.length}&from=0&to=4`);
+    return this.http.get<any>(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${searchArray}searchURL&limitLicense=false&number=12&ranking=2`);
   }
 
 
