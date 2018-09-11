54e95db2cd1bc1af7948790ed7ef2b678dfdc2db
joeldevos2008 <joeldevos2008@gmail.com>
2018-09-11 16:36:25 -0400

    some stuff at 4:36pm

--- RecipeApp/src/main/java/com/ex/controller/UserLoginController.java
+++ RecipeApp/src/main/java/com/ex/controller/UserLoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,20 +17,24 @@ import com.ex.services.UserLoginService;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "*")
+//(origins = "http://localhost:4200")
 public class UserLoginController {
-	
+
 	@Autowired
 	private UserLoginService userLoginService;
-	
-	@RequestMapping(value="/create", method=RequestMethod.POST,
-			consumes=MediaType.APPLICATION_JSON_VALUE,
-			produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserLogin> addUserLogin(@RequestBody UserLogin a) {
+		
 		System.out.println("woohoo! we made it here!");
+		System.out.println(a);
+		
 		a = userLoginService.addUserLogin(a);
-		if(a == null) {
+		
+		if (a == null) {
 			return new ResponseEntity<UserLogin>(HttpStatus.CONFLICT);
-		}
+		} 
 		else {
 			return new ResponseEntity<UserLogin>(a, HttpStatus.CREATED);
 		}
