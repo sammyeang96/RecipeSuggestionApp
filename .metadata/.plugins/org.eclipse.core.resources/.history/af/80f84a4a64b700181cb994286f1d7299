61b1b8eb7ed323d0752ecf33b20c1027779ab8f9
10michael30 <mikeclutch@yahoo.com>
2018-09-13 10:52:58 -0400

    k

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/af/80f84a4a64b700181cb994286f1d7299
@@ -0,0 +1,9 @@
+package com.ex.repositories;
+
+import com.ex.beans.UserInformation;
+
+public interface UserInformationRepository {
+	
+	public UserInformation add(UserInformation a);
+
+}
--- Backend/src/main/java/com/ex/repositories/UserInformationRepository.java
+++ Backend/src/main/java/com/ex/repositories/UserInformationRepository.java
@@ -1,9 +1,9 @@
 package com.ex.repositories;
 
-import com.ex.beans.UserInformation;
+import com.ex.beans.UserInfo;
 
 public interface UserInformationRepository {
 	
-	public UserInformation add(UserInformation a);
+	public UserInfo add(UserInfo a);
 
 }
